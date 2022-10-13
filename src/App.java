9af7918e45174a7474b9a3cf69aab681b97b01c7
Andrés Millán <andres.millan-munoz@t-systems.com>
2022-10-13 12:02:52 +0200

    Sistema completado

--- src/App.java
+++ src/App.java
@@ -12,12 +12,9 @@ import javax.security.auth.login.LoginException;
 
 enum MenuOptions {
     NOOP(0),
-    BOOK(1),
-    CANCEL(2),
-    LIST(3),
-    REGISTER_USER(4),
-    LOGIN_USER(5),
-    EXIT(6);
+    REGISTER_USER(1),
+    LOGIN_USER(2),
+    EXIT(3);
 
     private final int value;
 
@@ -40,20 +37,16 @@ enum MenuOptions {
     }
 
     public static void printMenu() {
-        System.out.println("   1) Reservar entradas");
-        System.out.println("   2) Anular una reserva");
-        System.out.println("   3) Listar una reserva");
-        System.out.println("   4) Registrarse");
-        System.out.println("   5) Login");
-        System.out.println("   6) Salir");
+        System.out.println("   1) Registrarse");
+        System.out.println("   2) Login");
+        System.out.println("   3) Salir");
     }
 }
 
 
 public class App {
-    static Baptisterio baptisterio = new Baptisterio();
-    static UserDB userDB           = new UserDB();
-    static User user               = null;
+    static UserDB userDB = new UserDB();
+    static User user     = null;
 
     public static void main(String[] args) throws Exception {
         var       option = MenuOptions.NOOP;
@@ -61,9 +54,7 @@ public class App {
         Exception error  = null;
 
         do {
-            if (option != MenuOptions.LIST) {
-                clearScreen();
-            }
+            clearScreen();
 
             if (error != null) {
                 System.err.println("Se ha producido un error. Motivo: " + error.getMessage());
@@ -93,28 +84,15 @@ public class App {
             }
 
             switch (option) {
-                case BOOK -> {
+                case REGISTER_USER -> {
                     try {
-                        book(scan);
-                    }
-                    catch (DateTimeException e) {
-                        error = e;
+                        registerUser(scan);
                     }
                     catch (LoginException e) {
                         error = e;
                     }
                 }
-                case CANCEL -> {
-                    try {
-                        cancel(scan);
-                    }
-                    catch (LoginException e) {
-                        error = e;
-                    }
-                }
-                case LIST            -> baptisterio.printReservations();
-                case REGISTER_USER   -> registerUser(scan);
-                case LOGIN_USER      -> {
+                case LOGIN_USER -> {
                     try {
                         login(scan);
                     }
@@ -122,8 +100,8 @@ public class App {
                         error = e;
                     }
                 }
-                case EXIT            -> System.out.println("¡Hasta luego!");
-                case NOOP            -> {}
+                case EXIT -> System.out.println("¡Hasta luego!");
+                case NOOP -> {}
             }
         } while (option != MenuOptions.EXIT);
 
@@ -132,24 +110,6 @@ public class App {
         if (!userDB.writeDB()) {
             System.err.println("Ha habido errores escribiendo la base de datos");
         }
-    }
-
-
-    private static LocalDateWrapper readDate(Scanner scan) throws DateTimeException{
-        System.out.println("¿Qué día quieres comprobar? (YYYY-MM-DD)");
-        System.out.print("> ");
-
-        String dateString = scan.nextLine();
-        LocalDate date;
-
-        try {
-            date = LocalDate.parse(dateString);
-        }
-        catch (DateTimeParseException e) {
-            throw new DateTimeException("La fecha introducida es inválida");
-        }
-
-        return new LocalDateWrapper(date);
     }
 
 
@@ -173,7 +133,8 @@ public class App {
         return true;
     }
 
-    private static boolean registerUser(Scanner scan){
+
+    private static boolean registerUser(Scanner scan) throws LoginException {
         System.out.println("Escriba un usuario");
         System.out.print("> ");
         String name = scan.nextLine();
@@ -192,92 +153,12 @@ public class App {
         boolean addedCorrectly = userDB.addUser(possibleUser);
 
         if (!addedCorrectly) {
-            throw new InputMismatchException("El usuario ya existe");
+            throw new LoginException("El usuario ya existe");
         }
 
         user = possibleUser;
 
         return addedCorrectly;
-    }
-
-
-    private static void book (Scanner scan)
-        throws Exception, DateTimeException, LoginException
-    {
-        if (user == null) {
-            throw new LoginException("Debes iniciar sesión para reservar las entradas.");
-        }
-
-        LocalDateWrapper date = null;
-
-        try {
-            date = readDate(scan);
-        }
-        catch (DateTimeException e) {
-            throw e;
-        }
-
-        if (date.isBefore(LocalDate.now())) {
-            throw new DateTimeException("No puedes reservar entradas para días pasados");
-        }
-        if (!baptisterio.avaliable(date)) {
-            throw new Exception("La fecha introducida no está disponible");
-        }
-
-        int round     = 0;
-        int remaining = baptisterio.PRICE;
-        ArrayList<CoinEuro> coins = new ArrayList<CoinEuro>();
-
-
-        do {
-            clearScreen();
-            System.out.println("Debes abonar " + baptisterio.PRICE + " céntimos.");
-            System.out.println("Introduce las monedas en el sistema. Puedes separarlas por comas o introducirlas poco a poco");
-            System.out.print(remaining + " céntimos restantes. \n> ");
-
-            String coinsString = scan.nextLine();
-            var coinsRound = CoinEuro.fromString(coinsString);
-            coins.addAll(coinsRound);
-
-            round      = CoinEuro.sum(coinsRound);
-            remaining -= round;
-
-        } while (round <= remaining);
-
-        if (remaining < 0) {
-            System.out.println("\nTe has pasado " + Math.abs(remaining) + " céntimos.\n(Te imaginas que te he dado el cambio porque esto es un programa cutre).");
-            coins.remove(CoinEuro.fromValue(Math.abs(remaining)));
-        }
-
-        baptisterio.pay(user, date, coins);
-        System.out.println("Su pago se ha procesado correctamente");
-
-        Thread.sleep(2000);
-    }
-
-
-    private static void cancel (Scanner scan) throws InterruptedException, LoginException {
-        if (user == null) {
-            throw new LoginException("Debes iniciar sesión para reservar las entradas.");
-        }
-
-        LocalDateWrapper date = null;
-
-        try {
-            date = readDate(scan);
-        }
-        catch (DateTimeException e) {
-            throw e;
-        }
-
-        if (baptisterio.cancel(user, date)) {
-            System.out.println("Su cita se ha cancelado con éxito!");
-        }
-        else {
-            System.out.println("No se ha podido cancelar su reserva.");
-        }
-
-        Thread.sleep(2000);
     }
 
 
